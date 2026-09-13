b16a6a52c09b0e5c595cafab0ceb4f8cec21762d
xin zong <36766561+zongxin1993@users.noreply.github.com>
2026-09-13 14:18:14 +0800

    Tests: fix Testcontainers pytest markers

--- tests/integration/test_testcontainers_postgres.py
+++ tests/integration/test_testcontainers_postgres.py
@@ -6,7 +6,7 @@ import pytest
 postgres_module = pytest.importorskip("testcontainers.postgres")
 PostgresContainer = postgres_module.PostgresContainer
 
-pytestmark = (pytest.mark.integration, pytest.mark.external, pytest.mark.postgres)
+pytestmark = [pytest.mark.integration, pytest.mark.external, pytest.mark.postgres]
 
 POSTGRES_IMAGE = "postgres:18.6@sha256:4ef4dbc939d61acea57712655ddb4b4ab27419c913f94cca0cd57cb3ea3c2280"
 
